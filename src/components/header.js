bb5a415f7b4dafa5a26d3f495f8a9c5e31b6264a
Abdulrahman Hesham <abdelrahman.rihan@ejust.edu.eg>
2024-07-19 16:17:17 +0300

    hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,29 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link";
 import logo from "./logo.png"
 
+import { useRef } from 'react';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+function showorhidemenu(){
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+}
+
 export default function Header(){
+
+    const navMenuRef = useRef(null);
+
+    const toggleNavMenu = () => {
+        if (navMenuRef.current) {
+        navMenuRef.current.classList.toggle('hidden');
+        }
+    };
+
     return(
         <main>
             <div class="hidden md:flex flex-row w-full m-auto p-4 bg-dark">
@@ -15,9 +36,13 @@ export default function Header(){
                     <Link href="/login"><button class="bg-blue rounded-md py-2 px-4">Customer Login</button></Link>
                 </div>
             </div>
-            <div class="flex md:hidden flex-col md:flex-row w-full m-auto p-4 bg-dark">
-                <Image class="w-36 h-18 md:mx-0 mx-auto" src={logo} alt="logo"/>
-                <div class="flex flex-col md:flex-row flex-1 justify-around my-auto md:text-left text-center">
+            <div class="flex md:hidden flex-col md:flex-row w-full m-auto  bg-dark">
+                <div class="flex flex-row justify-between p-4">
+                    <Image class="w-36 h-18 md:mx-0 mx-auto" src={logo} alt="logo"/>
+                    <button onClick={toggleNavMenu}><FontAwesomeIcon class="w-6 h-6 my-auto" icon={faBars} /></button>
+                </div>
+                <hr class="border border-gray"/>
+                <div ref={navMenuRef} class="hidden flex flex-col md:flex-row flex-1 justify-around my-auto md:text-left text-center p-4">
                     <Link href="/"><button class="p-2 md:my-0 my-2">Home</button></Link>
                     <Link href="/products"><button class="p-2 md:my-0 my-2">Products</button></Link>
                     <Link href="/about"><button class="p-2 md:my-0 my-2">About Us</button></Link>
